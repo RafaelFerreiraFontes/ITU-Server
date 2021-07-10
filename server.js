d3bb1050f66acb151e802b8c8e3590bcc1bafceb
Rafael Ferreira Fontes <50003237+RafaelFerreiraFontes@users.noreply.github.com>
2021-07-10 18:56:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const port = 8080 || process.env.PORT;
 
 const mongoose = require('mongoose');
 
-const uri = process.env.DB_URI;
-
 mongoose.Promise = global.Promise;
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pm4ek.gcp.mongodb.net/CommentsDB?retryWrites=true&w=majority`, {
