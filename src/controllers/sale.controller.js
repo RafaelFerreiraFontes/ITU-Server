6f5fa16007fc2e6f98cb87e03a55fa26379a41fe
DavidMPN <david.manuel27@hotmail.com>
2021-09-17 11:07:10 -0300

    add sale table

--- src/controllers/sale.controller.js
+++ src/controllers/sale.controller.js
@@ -1,7 +1,14 @@
 const sale = require("../models/sale.model");
 
 class SaleController {
-    
+    add(products, userId){
+        const productIds = products.map(prod => prod.id);
+
+        sale.create({productId: productIds, userId: userId}, function (err, data) {
+            if (err) console.log(err)
+            else console.log("Deu bom")
+        });
+    }
 }
 
 module.exports = new SaleController;
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const user      = require("../models/user.model");
 const product   = require("../models/product.model");
-const sale      = require("../models/sale.model");
+const saleController  = require("./sale.controller");
 const nodemailer = require('nodemailer');
 
 class UserController {
@@ -10,10 +10,16 @@ class UserController {
         user.findOne({ email: userData.email }, (err, data) => {
             if(err) throw err;
 
+            let user_id
+
             if(!data) {
                 // create user
-                user.create(userData).catch(err => res.json({ message: "erro"}));
-                return;
+                user.create(userData, (err,data) => {
+                    if(err) res.json({ message: "erro"});
+                    else user_id = data._id
+                })
+            }else{
+                user_id = data._id
             }
 
             // verify productCart
@@ -38,31 +44,30 @@ class UserController {
 
                     
                 } else {
+                    saleController.add(productCart,user_id);
+
+
                     productCart.forEach(async prod => {
                         const prodStock = data.find(prodStock => prodStock._id == prod.id);
                         await product.updateOne({ _id: prod.id }, { quant: prodStock.quant - prod.quant });
                     });
 
+                    
                     res.json({ message: "compra realizada" });
-                    // [o] remover do db (vulgo diminuir a coluna quant)
-                    
-                    
-                    
-                    // [x/4] enviar email
-                    
+
                     let transporter = nodemailer.createTransport({
                         service: "gmail",
                         auth: {
                             user: process.env.COMPANY_EMAIL,
                             pass:  process.env.COMPANY_EMAIL_PASS
-                          },
+                            },
                     });
 
                     let message = {
                         from: process.env.COMPANY_EMAIL,
                         to: userData.email,
                         subject: "ITU - Sua compra foi realizada com successo!",
-                        text: `Parabéns ${userData.name}, pela sua nova aquisição. Nesse e-mail segue as instruções para acesso ao(s) jogo(s) comprado(s):\nO(s) jogo(s) e sua(s) respectiva(s) chave(s):\n${productCart.reduce((acum, elem) => acum + `${elem.title} - ${elem.id}\n`, "")}\nValor total: ${productCart.reduce((acum, elem) => acum + elem.quant * elem.price, 0)}\nO pagamento deverá ser feito via Pix na chave: 44a9b2ea-7460-4c9e-914a-fb4c3da57f0b.\nObrigado pela preferência e aproveite seus jogos.`
+                        text: `Parabéns ${userData.name}, pela sua nova aquisição. Nesse e-mail segue as instruções para acesso ao(s) jogo(s) comprado(s):\nO(s) jogo(s) e sua(s) respectiva(s) chave(s):\n${productCart.reduce((acum, elem) => acum + `${elem.title} - ${elem.id}\n`, "")}\nValor total: ${(productCart.reduce((acum, elem) => acum + elem.quant * elem.price, 0)).toFixed(2)}\nO pagamento deverá ser feito via Pix na chave: 44a9b2ea-7460-4c9e-914a-fb4c3da57f0b.\nObrigado pela preferência e aproveite seus jogos.`
                     }
                     
                     
